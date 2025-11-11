2d3453775b9e4852e710b09374e9e20aa35acf07
sooryaz <sooryanandpandith31@gmail.com>
2025-11-11 13:06:24 +0530

    Server updated for structured messages

--- server.js
+++ server.js
@@ -8,14 +8,21 @@ app.use(express.static(__dirname));
 io.on("connection", (socket) => {
   console.log("a user connected");
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  // Store user identity when they join
+  socket.on("join", ({ name, color }) => {
+    socket.data.user = { name, color };
+  });
+
+  // Broadcast messages with structured data
+  socket.on("chat message", (m) => {
+    io.emit("chat message", m);
   });
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 });
+
 
 // Change HERE:
 http.listen(3000, "0.0.0.0", () => {
