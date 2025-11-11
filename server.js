e35e5ba3c6204b0ab566bb22177d6e2f5558e4a8
sooryaz <sooryanandpandith31@gmail.com>
2025-11-11 10:20:14 +0530

    Initial upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static(__dirname));
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+// Change HERE:
+http.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on http://0.0.0.0:3000");
+});
